572151bf4c05e7d1c809fb5e4b39915f977349a5
IgorT1 <106947961+IgorT1@users.noreply.github.com>
2023-04-03 08:43:49 +0300

    Update FileName.cpp

--- FileName.cpp
+++ FileName.cpp
@@ -1,64 +1,9 @@
-//#include <iostream>
-//#include <string>
-//using namespace std;
-//
-//int mod(int a, int b) {
-//    return (a % b + b) % b;
-//}
-//
-//int gcd(int a, int b) {// находим обший делитель 
-//    if (b == 0) {
-//        return a;
-//    }
-//    return gcd(b, a % b);
-//}
-//
-//int modInverse(int a, int m) {//инверсируем значение по целочислености 
-//    a = mod(a, m);
-//    for (int x = 1; x < m; x++) {
-//        if (mod(a * x, m) == 1) {
-//            return x;
-//        }
-//    }
-//    return -1;
-//}
-//
-//void decrypt(string ciphertext, int a, int b) {
-//    string plaintext = "";  
-//    int a_inverse = modInverse(a, 26);
-//    for (int i = 0; i < ciphertext.length(); i++) {
-//        if (isalpha(ciphertext[i])) {
-//            char c = tolower(ciphertext[i]);
-//            int d = mod(a_inverse * (c - 'a' - b), 26);
-//            plaintext += char(d + 'a');
-//        }
-//        else {
-//            plaintext += ciphertext[i];
-//        }
-//    }
-//    cout << "Key: a = " << a << ", b = " << b << endl;
-//    cout << "Plaintext: " << plaintext << endl;
-//}
-//
-//int main() {
-//    string ciphertext = "jzq kuudncrejqg ut jzq jedwai jus tumd qlqpqr";
-//    for (int a = 3; a < 5; a++) {//пол байта размер шифратора 
-//        for (int b = 4; b < 5; b++) {
-//            if (gcd(a, 26) == 1) {
-//                decrypt(ciphertext, a, b);
-//            }
-//        }
-//    }
-//    return 0;
-//}
-
-
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int modInverse(int a, int m) {//вычисляет обратный элемент по модулю для заданного a и m
+int modInverse(int a, int m) {//ГўГ»Г·ГЁГ±Г«ГїГҐГІ Г®ГЎГ°Г ГІГ­Г»Г© ГЅГ«ГҐГ¬ГҐГ­ГІ ГЇГ® Г¬Г®Г¤ГіГ«Гѕ Г¤Г«Гї Г§Г Г¤Г Г­Г­Г®ГЈГ® a ГЁ m
     a = a % m;
     for (int x = 1; x < m; x++) {
         if ((a * x) % m == 1) {
@@ -68,17 +13,17 @@ int modInverse(int a, int m) {//вычисляет обратный элемент по модулю для заданно
     return 1;
 }
 
-string affineDecrypt(string ciphertext, int a, int b) {// принимает зашифрованный текст а и b и возвращает расшифрованный текст
+string affineDecrypt(string ciphertext, int a, int b) {// ГЇГ°ГЁГ­ГЁГ¬Г ГҐГІ Г§Г ГёГЁГґГ°Г®ГўГ Г­Г­Г»Г© ГІГҐГЄГ±ГІ Г  ГЁ b ГЁ ГўГ®Г§ГўГ°Г Г№Г ГҐГІ Г°Г Г±ГёГЁГґГ°Г®ГўГ Г­Г­Г»Г© ГІГҐГЄГ±ГІ
     string plaintext = "";
-    int a_inv = modInverse(a, 26);//передает значение для вычисление элемента по модулю
+    int a_inv = modInverse(a, 26);//ГЇГҐГ°ГҐГ¤Г ГҐГІ Г§Г­Г Г·ГҐГ­ГЁГҐ Г¤Г«Гї ГўГ»Г·ГЁГ±Г«ГҐГ­ГЁГҐ ГЅГ«ГҐГ¬ГҐГ­ГІГ  ГЇГ® Г¬Г®Г¤ГіГ«Гѕ
     for (char c : ciphertext) {
-        if (c >= 'a' && c <= 'z') { //провекра является ли буква в нижнем регистре 
-            int x = (a_inv * (c - 'a' - b + 26)) % 26;//разшифровка текста 
-            plaintext += (char)(x + 'a');//добавление елемента к строке 
+        if (c >= 'a' && c <= 'z') { //ГЇГ°Г®ГўГҐГЄГ°Г  ГїГўГ«ГїГҐГІГ±Гї Г«ГЁ ГЎГіГЄГўГ  Гў Г­ГЁГ¦Г­ГҐГ¬ Г°ГҐГЈГЁГ±ГІГ°ГҐ 
+            int x = (a_inv * (c - 'a' - b + 26)) % 26;//Г°Г Г§ГёГЁГґГ°Г®ГўГЄГ  ГІГҐГЄГ±ГІГ  
+            plaintext += (char)(x + 'a');//Г¤Г®ГЎГ ГўГ«ГҐГ­ГЁГҐ ГҐГ«ГҐГ¬ГҐГ­ГІГ  ГЄ Г±ГІГ°Г®ГЄГҐ 
         }
         else {
-            tolower(c);//переводим в нижнйи регистр 
-            plaintext += c;//добавляем к строке 
+            tolower(c);//ГЇГҐГ°ГҐГўГ®Г¤ГЁГ¬ Гў Г­ГЁГ¦Г­Г©ГЁ Г°ГҐГЈГЁГ±ГІГ° 
+            plaintext += c;//Г¤Г®ГЎГ ГўГ«ГїГҐГ¬ ГЄ Г±ГІГ°Г®ГЄГҐ 
         }
     }
     return plaintext;
@@ -88,12 +33,12 @@ int main() {
     string ciphertext = "jzq kuudncrejqg ut jzq jedwai jus tumd qlqpqr";
     string target = "target";
 
-    for (int a = 3; a < 5; a ++) { // ключи пол байка (4)
+    for (int a = 3; a < 5; a ++) { // ГЄГ«ГѕГ·ГЁ ГЇГ®Г« ГЎГ Г©ГЄГ  (4)
         int a_inv = modInverse(a, 26);
-        for (int b = 3; b < 5; b++) {//также 
+        for (int b = 3; b < 5; b++) {//ГІГ ГЄГ¦ГҐ 
             string plaintext = affineDecrypt(ciphertext, a, b);
-                cout << "Найден ключ: a = " << a << ", b = " << b << endl;
-                cout << "Открытый текст: " << plaintext << endl;
+                cout << "ГЌГ Г©Г¤ГҐГ­ ГЄГ«ГѕГ·: a = " << a << ", b = " << b << endl;
+                cout << "ГЋГІГЄГ°Г»ГІГ»Г© ГІГҐГЄГ±ГІ: " << plaintext << endl;
             
         }
     }
